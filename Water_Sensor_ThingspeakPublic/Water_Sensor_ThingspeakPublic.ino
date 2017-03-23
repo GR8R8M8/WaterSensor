90185b6700dfd4d8fd4e189024d7bbc5c46c62f3
GR8R8M8 <Nickbroza@gmail.com>
2017-03-22 22:13:32 -0400

    Add files via upload

---
+++ Water_Sensor_ThingspeakPublic/Water_Sensor_ThingspeakPublic.ino
@@ -0,0 +1,48 @@
+#include "ThingSpeak.h"
+#include <ESP8266WiFi.h>
+
+//Define LED Pins
+int ledGreenPin = D0;
+int ledRedPin = D1;
+
+//Define Water Threshold
+int waterThreshold = 300; 
+
+//WiFi Setup
+char ssid[] = "ssid";    // Replace ssid with your network name
+char pass[] = "password";   // Replace password with your network password
+int status = WL_IDLE_STATUS;
+WiFiClient  client;
+
+//Thingspeak Setup
+unsigned long myChannelNumber = 123456;
+const char * myWriteAPIKey = "apikey";
+
+//Startup Code
+void setup() {
+  WiFi.begin(ssid, pass);
+  ThingSpeak.begin(client);
+  pinMode(ledGreenPin, OUTPUT);
+  pinMode(ledRedPin, OUTPUT);
+}
+
+void loop() {
+  
+  if (analogRead(A0)> waterThreshold)
+   {
+    digitalWrite(ledGreenPin, LOW);
+    digitalWrite(ledRedPin, HIGH);
+   }
+  else 
+   {
+    digitalWrite(ledRedPin, LOW);
+    digitalWrite(ledGreenPin, HIGH);
+   }
+ 
+  float w = analogRead(A0);
+
+  ThingSpeak.setField(1,w);
+  ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  
+  delay(20000);
+}
